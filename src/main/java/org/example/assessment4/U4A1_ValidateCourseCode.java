9b9c1c7289fe4defb141345e11c0ae4bbd418159
StaceyO21 <staceyobrien21@gmail.com>
2025-05-08 21:35:47 -0500

    assessment 4 readme

--- src/main/java/org/example/assessment4/U4A1_ValidateCourseCode.java
+++ src/main/java/org/example/assessment4/U4A1_ValidateCourseCode.java
@@ -1,4 +1,9 @@
 package org.example.assessment4;
 
 public class U4A1_ValidateCourseCode {
+
+    String courseLetter = "IT";
+    String courseLetter2 = courseLetter.toUpperCase();
+
+//    Integer courseNum =
 }
