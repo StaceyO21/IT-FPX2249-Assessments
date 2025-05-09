95a6fae0911d31c608efa7375146afd8c816dba5
StaceyO21 <staceyobrien21@gmail.com>
2025-05-08 21:52:05 -0500

    needs fixing

--- src/main/java/org/example/assessment4/U4A1_ValidateCourseCode.java
+++ src/main/java/org/example/assessment4/U4A1_ValidateCourseCode.java
@@ -1,9 +1,17 @@
 package org.example.assessment4;
 
+import java.util.Random;
+
 public class U4A1_ValidateCourseCode {
 
     String courseLetter = "IT";
     String courseLetter2 = courseLetter.toUpperCase();
 
-//    Integer courseNum =
+    String[] courseNum = {}
+    for(int i = 0; i < 5; i++){
+
+
+        int randomInt(0,10);
+    }
+
 }
