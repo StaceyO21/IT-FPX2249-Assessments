edc3cd1109efaaadcc4cd60e1347684d928e2864
StaceyO21 <staceyobrien21@gmail.com>
2025-05-11 19:24:33 -0500

    finished assessment

--- src/main/java/org/example/assessment4/U4A1_ValidateCourseCode.java
+++ src/main/java/org/example/assessment4/U4A1_ValidateCourseCode.java
@@ -7,19 +7,10 @@ import java.util.Scanner;
 public class U4A1_ValidateCourseCode {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter a course code: ");
-
-        Random random = new Random();
-
-        String courseLetter = "IT";
-        String courseCode = courseLetter.toUpperCase();
-
-        for (int i = 0; i < 4; i++) {
-            int digit = random.nextInt(10);
-            courseCode += digit;
-        }
-        System.out.println("Course Code: " + courseCode);
-
+        String courseCode = scanner.nextLine();
 
         validateCoursecode(courseCode);
 
@@ -29,6 +20,30 @@ public class U4A1_ValidateCourseCode {
     private static void validateCoursecode(String code) {
         if (code.length() != 6) {
             System.out.println("Invalid course code: Must be 6 characters long");
+            return;
         }
+
+        char firstChar = code.charAt(0);
+        if (!(firstChar == 'I' || firstChar == 'i')) {
+            System.out.println("Invalid course code: First character must be the letter I");
+            return;
+        }
+
+        char secondChar = code.charAt(1);
+        if (!(secondChar == 'T' || secondChar == 't')) {
+            System.out.println("Invalid course code: Second character must be the letter T");
+            return;
+        }
+
+        for (int i = 2; i < 6; i++) {
+            char digit = code.charAt(i);
+            if(!Character.isDigit(digit)) {
+                System.out.println("Invalid course code: Characters 3-6 must be digits (0-9)");
+                return;
+            }
+
+        }
+
+            System.out.println("Course code is valid");
     }
 }
