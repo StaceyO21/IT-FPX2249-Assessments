dc13984471840814a8759b628a6e1b7519547f84
StaceyO21 <staceyobrien21@gmail.com>
2025-05-15 20:40:15 -0500

    code written. just fix changes

--- src/main/java/org/example/assessment5/U5A1_HighestAndLowestFiveIntegers.java
+++ src/main/java/org/example/assessment5/U5A1_HighestAndLowestFiveIntegers.java
@@ -1,7 +1,35 @@
 package org.example.assessment5;
 
+import java.util.Scanner;
+
 public class U5A1_HighestAndLowestFiveIntegers {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Teacher's Copy");
+        System.out.print("Enter 5 seperate integers: ");
+
+        int[] nums = new int[]{scanner.nextInt()};
+
+        int lowestNum = nums[0];
+        int highestNum = nums[0];
+
+        for (int num : nums){
+            if (lowestNum > num) {
+                lowestNum = num;
+            }
+            System.out.println("Lowest integer is " + lowestNum);
+
+            for (int num2 : nums) {
+                if (highestNum < num2) {
+                    highestNum = num2;
+                }
+                System.out.println("Highest integer is " + highestNum);
+            }
+        }
+
+
+    }
 
 }
