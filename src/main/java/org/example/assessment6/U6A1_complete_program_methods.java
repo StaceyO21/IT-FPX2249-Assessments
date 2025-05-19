7fac63c9d9f15917b0c5f72d2ac2138dcbfc1777
StaceyO21 <staceyobrien21@gmail.com>
2025-05-19 18:36:01 -0500

    changed to switch statement

--- src/main/java/org/example/assessment6/U6A1_complete_program_methods.java
+++ src/main/java/org/example/assessment6/U6A1_complete_program_methods.java
@@ -4,39 +4,48 @@ import java.util.Scanner;
 
 public class U6A1_complete_program_methods {
 
-    public static void printCourseNumber() {
-        System.out.println("Teacher's Copy");
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Teacher's Copy");
 
-        String courseChoice;
+        // print course information
+        System.out.println("Type the number inside the [] to register for the course\n" +
+                "[1] IT4782\n" +
+                "[2] IT4784\n" +
+                "[3] IT4786\n" +
+                "[4] IT4789\n" +
+                "[5] IT2230\n" +
+                "[6] IT3345\n" +
+                "[7] IT3349\n" +
+                "Enter your choice: ");
+
+
+        // get user input
         int choice = input.nextInt();
-        System.out.println("Type the number inside the [] to register for the coure" +
-                "[1] IT4782" +
-                "[2] IT4784" +
-                "[3] IT4786" +
-                "[4] IT4789" +
-                "[5] IT2230" +
-                "[6] IT3345" +
-                "[7] IT3349" +
-                "Enter your choice: " + choice);
 
-
-        if (choice == 1) {
-            courseChoice = "IT4782";
-        } else if (choice == 2) {
-            courseChoice = "IT4784";
-        } else if (choice == 3) {
-            courseChoice = "IT4786";
-        } else if (choice == 4) {
-            courseChoice = "IT4789";
-        } else if (choice == 5) {
-            courseChoice = "IT2230";
-        } else if (choice == 6) {
-            courseChoice = "IT3345";
-        } else {
-            courseChoice = "IT3349";
+        //validate choice
+        if(choice < 1 || choice > 7){
+            System.out.println("**Invalid** - Your selection of is not a recognized course.");
+            return;
         }
+
+        String courseChoice = getCourseCode(choice);
+
         System.out.println("Registration Confirmed for course " + courseChoice);
         System.out.println("Current course registration: {" + courseChoice + "}" );
+    }
+
+    public static String getCourseCode(int choice) {
+        // switch statement to determine users choice to the course number
+        switch (choice) {
+            case 1: return "IT4782";
+            case 2: return "IT4784";
+            case 3: return "IT4786";
+            case 4: return "IT4789";
+            case 5: return "IT2230";
+            case 6: return "IT3345";
+            case 7: return "IT3349";
+            default: return "Unknown"; // this shouldn't happen if validated
+        }
     }
 }
