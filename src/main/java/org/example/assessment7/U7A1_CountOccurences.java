f82919a77573e813cc6543448d1df8333e208bc8
StaceyO21 <staceyobrien21@gmail.com>
2025-05-20 20:25:54 -0500

    create user array

--- src/main/java/org/example/assessment7/U7A1_CountOccurences.java
+++ src/main/java/org/example/assessment7/U7A1_CountOccurences.java
@@ -1,8 +1,20 @@
 package org.example.assessment7;
 
+import java.util.Scanner;
+
 public class U7A1_CountOccurences {
 
-    public static void main(String[] args) {
+        public static int[] numbers = new int[7];  //array of numbers user inputs
 
-    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Teacher's Copy");
+
+        System.out.println("Enter 7 integers: ");
+        int userInput = input.nextInt();  //accept user numbers
+
+        for (int i = 0; i < 7; i++){
+            numbers[i] = input.nextInt();  //add user's numbers to the array
+        }
+
 }
