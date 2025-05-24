9ce36e32ddc9251c5d56f067efd0e9e60537b27b
StaceyO21 <staceyobrien21@gmail.com>
2025-05-23 19:24:43 -0500

    add countfreq method

--- src/main/java/org/example/assessment7/U7A1_CountOccurences.java
+++ src/main/java/org/example/assessment7/U7A1_CountOccurences.java
@@ -4,17 +4,41 @@ import java.util.Scanner;
 
 public class U7A1_CountOccurences {
 
-        public static int[] numbers = new int[7];  //array of numbers user inputs
+        public static int[] numbers = new int[7]; //array of numbers user inputs
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Teacher's Copy");
 
+        int n = numbers.length;
+        countFreq(numbers, n);
+
         System.out.println("Enter 7 integers: ");
         int userInput = input.nextInt();  //accept user numbers
 
-        for (int i = 0; i < 7; i++){
-            numbers[i] = input.nextInt();  //add user's numbers to the array
+        for (int i = 0; i < 7; i++) {
+            numbers[i] = userInput;  //add user's numbers to the array
         }
+
+    }
+
+    private static void countFreq(int[] numbers, int n) {
+
+        boolean visited[] = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == true) {
+                continue;
+            }
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (numbers[i] == numbers[j]) {
+                    visited[j] = true;
+                    count++;
+                }
+            }
+            System.out.println("Number " + numbers[i] + " occurs " + count + " times");
+        }
+    }
 
 }
