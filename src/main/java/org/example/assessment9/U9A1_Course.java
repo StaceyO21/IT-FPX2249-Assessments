0aa6ba6829882664fb19bc19643c4c6b7a42859f
StaceyO21 <staceyobrien21@gmail.com>
2025-05-26 20:02:12 -0500

    created constructor for course

---
+++ src/main/java/org/example/assessment9/U9A1_Course.java
@@ -0,0 +1,13 @@
+package org.example.assessment9;
+
+public class U9A1_Course {
+
+    private String courseCode = "";
+    private int creditHours = 0;
+
+    public U9A1_Course(String courseCode, int creditHours) {
+        this.courseCode = courseCode;
+        this.creditHours = creditHours;
+    }
+
+}
--- src/main/java/org/example/assessment9/U9A1_DefineClassInstantiateObj.java
+++ src/main/java/org/example/assessment9/U9A1_DefineClassInstantiateObj.java
@@ -1,4 +1,15 @@
 package org.example.assessment9;
 
 public class U9A1_DefineClassInstantiateObj {
+
+    public static void main(String[] args) {
+        System.out.println("Teacher's Copy");
+        System.out.println("Course objects each have a code (e.g. IT1004) and credit hours (e.g. 6)");
+        System.out.println("the number inside the [] is the display number order");
+        System.out.println("The number inside the [] is the credit hours for the courses");
+
+        Course[] courses = {
+                new Course = ""
+        }
+    }
 }
