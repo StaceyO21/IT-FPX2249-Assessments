3048c5a87e7fad6a29b763c011e0f3ba1d1e2c03
StaceyO21 <staceyobrien21@gmail.com>
2025-05-26 20:14:03 -0500

    fixed bugs

--- src/main/java/org/example/assessment9/U9A1_DefineClassInstantiateObj.java
+++ src/main/java/org/example/assessment9/U9A1_DefineClassInstantiateObj.java
@@ -3,10 +3,6 @@ package org.example.assessment9;
 public class U9A1_DefineClassInstantiateObj {
 
     public static void main(String[] args) {
-        System.out.println("Teacher's Copy");
-        System.out.println("Course objects each have a code (e.g. IT1004) and credit hours (e.g. 6)");
-        System.out.println("the number inside the [] is the display number order");
-        System.out.println("The number inside the [] is the credit hours for the courses");
 
         Course[] courses = {
                 new Course ("IT1006", 6),
@@ -18,7 +14,7 @@ public class U9A1_DefineClassInstantiateObj {
                 new Course ("IT2249", 6),
         };
         for(int i = 0; i < 7; i++){
-            System.out.println("[" + (i + 1) +"]" + courses[i].getCourseCode() + " (" courses[i].getCreditHours() + ")");
+            System.out.println("[" + (i + 1) +"]" + courses[i].getCourseCode() + " (" + courses[i].getCreditHours() + ")");
         }
     }
 }
