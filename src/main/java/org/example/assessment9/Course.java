fcf864ca6478d3aaecf7107ea4f6dd98bde8c5e4
StaceyO21 <staceyobrien21@gmail.com>
2025-05-26 20:10:56 -0500

    for loop to display courses

--- src/main/java/org/example/assessment9/Course.java
+++ src/main/java/org/example/assessment9/Course.java
@@ -1,11 +1,11 @@
 package org.example.assessment9;
 
-public class U9A1_Course {
+public class Course {
 
     private String courseCode = "";
     private int creditHours = 0;
 
-    public U9A1_Course(String courseCode, int creditHours) {
+    public Course(String courseCode, int creditHours) {
         this.courseCode = courseCode;
         this.creditHours = creditHours;
     }
--- src/main/java/org/example/assessment9/U9A1_DefineClassInstantiateObj.java
+++ src/main/java/org/example/assessment9/U9A1_DefineClassInstantiateObj.java
@@ -9,7 +9,16 @@ public class U9A1_DefineClassInstantiateObj {
         System.out.println("The number inside the [] is the credit hours for the courses");
 
         Course[] courses = {
-                new Course = ""
+                new Course ("IT1006", 6),
+                new Course ("IT4782", 3),
+                new Course ("IT4789", 3),
+                new Course ("IT4079", 6),
+                new Course ("IT2230", 3),
+                new Course ("IT3345", 3),
+                new Course ("IT2249", 6),
+        };
+        for(int i = 0; i < 7; i++){
+            System.out.println("[" + (i + 1) +"]" + courses[i].getCourseCode() + " (" courses[i].getCreditHours() + ")");
         }
     }
 }
