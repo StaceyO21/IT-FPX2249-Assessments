cf69609061eb42debce2edc26471aca5715292e3
StaceyO21 <staceyobrien21@gmail.com>
2025-05-30 20:22:56 -0500

    fixed the getchoice method

--- src/main/java/org/example/assessment10/U10A1_OOConsoleRegisterForCourse.java
+++ src/main/java/org/example/assessment10/U10A1_OOConsoleRegisterForCourse.java
@@ -2,10 +2,12 @@ package org.example.assessment10;
 
 import org.example.assessment9.Course;
 
+import java.util.Scanner;
+
 public class U10A1_OOConsoleRegisterForCourse {
 
     public static void main(String[] args) {
-
+        // creating the courses array that holds the course objects
         Course[] courses = {
                 new Course("IT1006", 6),
                 new Course("IT4782", 3),
@@ -13,12 +15,24 @@ public class U10A1_OOConsoleRegisterForCourse {
                 new Course("IT4079", 6),
                 new Course("IT2230", 3),
                 new Course("IT3345", 3),
-                new Course("IT2249", 6)
+                new Course("IT2249", 6),
         };
 
-        for (int i = 0; i < 7; i++) {
-            System.out.println("[" + (i + 1) + "]" + courses[i].getCourseCode + "(" + courses[i].getCreditHours + ")");
+        int choice = getChoice(courses);
+    }
+        public static int getChoice(Course[] courses){
+            Scanner input = new Scanner(System.in);
+            System.out.println("Type the number inside the [] to register for a course");
+
+            for (int i = 0; i < 7; i++) {
+                System.out.println("[" + (i + 1) + "]" + courses[i].getCourseCode + "("+courses[i].getCreditHours+")");
+            }
+            System.out.println("Enter your course choice: ");
+
+            return input.nextInt();
         }
 
-        public static String currentRegistration(Course[] courses, int creditHours){
+        public static String currentRegistration (Course[]courses, int creditHours){
+
+        }
 
