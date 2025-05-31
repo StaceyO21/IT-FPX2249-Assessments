1da16365d825178ac045121c2c9bc39f73b4eb72
StaceyO21 <staceyobrien21@gmail.com>
2025-05-30 20:42:54 -0500

    created WriteCurrentRegistration method

--- src/main/java/org/example/assessment10/U10A1_OOConsoleRegisterForCourse.java
+++ src/main/java/org/example/assessment10/U10A1_OOConsoleRegisterForCourse.java
@@ -18,21 +18,44 @@ public class U10A1_OOConsoleRegisterForCourse {
                 new Course("IT2249", 6),
         };
 
+        // user selects a course
         int choice = getChoice(courses);
+
+        // get the selected course and credit
+        Course[] registeredCourses = { courses[choice - 1] };
+        int totalCredits = registeredCourses[0].getCreditHours();
+
+        // registration summary
+        WriteCurrentRegistration(registeredCourses, totalCredits);
     }
-        public static int getChoice(Course[] courses){
-            Scanner input = new Scanner(System.in);
-            System.out.println("Type the number inside the [] to register for a course");
 
-            for (int i = 0; i < 7; i++) {
-                System.out.println("[" + (i + 1) + "]" + courses[i].getCourseCode + "("+courses[i].getCreditHours+")");
+    // loops through the courses array to display the objects
+    public static int getChoice(Course[] courses) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type the number inside the [] to register for a course");
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println("[" + (i + 1) + "]" + courses[i].getCourseCode() + "(" + courses[i].getCreditHours() + ")");
+        }
+        System.out.println("Enter your course choice: ");
+
+        return input.nextInt();
+    }
+
+    // prints the list of courses that are registered in the specified format requested
+    // also prints the total credit hours
+    public static void WriteCurrentRegistration(Course[] registeredCourses, int creditHours) {
+
+        System.out.println("Current course registration: { ");
+        for (int i = 0; i < registeredCourses.length; i++) {
+            System.out.println(registeredCourses[i].getCourseCode());
+            if (i < registeredCourses.length - 1) {
+                System.out.println(" , ");
             }
-            System.out.println("Enter your course choice: ");
-
-            return input.nextInt();
         }
+        System.out.println(" } ");
+        System.out.println("Total registered credit hours: " + creditHours);
+    }
+}
 
-        public static String currentRegistration (Course[]courses, int creditHours){
-
-        }
 
